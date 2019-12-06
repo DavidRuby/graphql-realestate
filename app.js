dec5900247aa4b9dcc2832421460d61df6b25b07
Laura Urrea <Laura@Lauras-MacBook-Air.local>
2019-12-06 11:11:33 -0500

    commit 1

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+const events = [];
+
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        imput EventImput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventImput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventImput.title,
+          description: args.eventImput.description,
+          price: +args.eventImput.price,
+          date: args.eventImput.date,
+        };
+        console.log(args);
+        events.push(event);
+        return event;
+      }
+    },
+    graphiql: true
+  })
+);
+
+app.listen(8383);
