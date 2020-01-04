cb2cb97c89854e0c1a0aeffdc5b143aca274d5ef
Laura Urrea <Laura@Lauras-MacBook-Air.local>
2020-01-04 11:17:18 -0500

    noon

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', (req, res, next) => {
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
